4b2ceaed5ef43f86da468bdc3328178fc44c0c7f
William Chronowski <WC47833@pekininsurance.com>
2017-07-17 06:59:56 -0500

    Add Closed state

---
+++ StateDesignPattern.Core.Tests/ClosedTest.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace StateDesignPattern.Core.Tests {
+    public class ClosedTest {
+        [Fact]
+        public void Initialize() {
+            var state = BuildClosed();
+
+            Assert.NotNull(state);
+            Assert.IsAssignableFrom<IState>(state);
+        }
+
+        [Fact]
+        public void Freeze_ReturnsCurrentState() {
+            var state = BuildClosed();
+
+            var newState = state.Freeze();
+
+            Assert.Equal(state, newState);
+            Assert.IsType<Closed>(newState);
+        }
+
+        [Fact]
+        public void Deposit_ReturnsCurrentState()
+        {
+            var state = BuildClosed();
+
+            var newState = state.Deposit(() => new MockAction().Run());
+
+            Assert.Equal(state, newState);
+            Assert.IsType<Closed>(newState);
+        }
+
+        [Fact]
+        public void Withdraw_ReturnsCurrentState()
+        {
+            var state = BuildClosed();
+
+            var newState = state.Withdraw(() => new MockAction().Run());
+
+            Assert.Equal(state, newState);
+            Assert.IsType<Closed>(newState);
+        }
+
+        [Fact]
+        public void Closed_ReturnsCurrentState()
+        {
+            var state = BuildClosed();
+
+            var newState = state.Close();
+
+            Assert.Equal(state, newState);
+            Assert.IsType<Closed>(newState);
+        }
+
+        private static Closed BuildClosed() {
+            return new Closed();
+        }
+    }
+}
---
+++ StateDesignPattern.Core/Closed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StateDesignPattern.Core {
+    public class Closed : IState {
+        public IState Deposit(Action addToBalance) => this;
+
+        public IState Withdraw(Action removeFromBalance) => this;
+
+        public IState Freeze() => this;
+
+        public IState Close() => this;
+    }
+}
