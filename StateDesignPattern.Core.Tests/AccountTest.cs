ae0583d90afcb07003d1e2ad8b53f29c79663525
William Chronowski <WC47833@pekininsurance.com>
2017-07-17 07:41:15 -0500

    Change Account to use states

--- StateDesignPattern.Core.Tests/AccountTest.cs
+++ StateDesignPattern.Core.Tests/AccountTest.cs
@@ -9,39 +9,7 @@ namespace StateDesignPattern.Core.Tests {
 
             Assert.NotNull(account);
         }
-
-        [Fact]
-        public void Initialize_SetsIsVerified_False() {
-            var account = BuildAccount();
-
-            Assert.False(account.IsVerified);
-        }
-
-        [Fact]
-        public void Initialize_SetsIsClosed_False() {
-            var account = BuildAccount();
-
-            Assert.False(account.IsClosed);
-        }
-
-        [Fact]
-        public void HolderVerified_SetsIsVerified_True() {
-            var account = BuildAccount();
-
-            account.HolderVerified();
-
-            Assert.True(account.IsVerified);
-        }
-
-        [Fact]
-        public void Close_SetsIsClosed_True() {
-            var account = BuildAccount();
-
-            account.Close();
-
-            Assert.True(account.IsClosed);
-        }
-
+ 
         [Fact]
         public void Deposit_BalanceChangesWhenAccountIsOpen() {
             const decimal STARTING_AMOUNT = 10;
@@ -74,19 +42,7 @@ namespace StateDesignPattern.Core.Tests {
             account.Deposit(10);
 
             Assert.Equal(20, account.Balance);
-        }
-
-        [Fact]
-        public void Deposit_SetsIsFrozen_False() {
-            var action = new MockAction();
-            var account = BuildAccount(action: () => action.Run());
-
-            account.HolderVerified();
-            account.Freeze();
-            account.Deposit(10);
-
-            Assert.False(account.IsFrozen);
-        }
+        }  
 
         [Fact]
         public void Deposit_UnfreezeInvokedWhenAccountIsFrozen() {
@@ -146,18 +102,6 @@ namespace StateDesignPattern.Core.Tests {
         }
 
         [Fact]
-        public void Withdraw_SetsIsFrozen_False() {
-            var action = new MockAction();
-            var account = BuildAccount(action: () => action.Run());
-
-            account.HolderVerified();
-            account.Freeze();
-            account.Withdraw(5);
-
-            Assert.False(account.IsFrozen);
-        }
-
-        [Fact]
         public void Withdraw_UnfreezeInvokedWhenAccountIsFrozen() {
             var action = new MockAction();
             var account = BuildAccount(action: () => action.Run());
@@ -167,38 +111,6 @@ namespace StateDesignPattern.Core.Tests {
             account.Withdraw(5);
 
             action.VerifyRunCalled();
-        }
-
-        [Fact]
-        public void Freeze_AccountFrozenWhenNotClosedAndVerified() {
-            var account = BuildAccount();
-
-            account.HolderVerified();
-            account.Freeze();
-
-            Assert.True(account.IsFrozen);
-        }
-
-        [Fact]
-        public void Freeze_AccountNotFrozenWhenClosed() {
-            const decimal STARTING_AMOUNT = 10;
-            var account = BuildAccount(STARTING_AMOUNT);
-
-            account.Close();
-            account.HolderVerified();
-            account.Freeze();
-
-            Assert.False(account.IsFrozen);
-        }
-
-        [Fact]
-        public void Freeze_AccountNotFrozenWhenNotVerified() {
-            const decimal STARTING_AMOUNT = 10;
-            var account = BuildAccount(STARTING_AMOUNT);
-
-            account.Freeze();
-
-            Assert.False(account.IsFrozen);
         }
 
         private static Account BuildAccount(decimal? startingBalance = null, Action action = null) {
--- StateDesignPattern.Core/Account.cs
+++ StateDesignPattern.Core/Account.cs
@@ -2,60 +2,32 @@
 
 namespace StateDesignPattern.Core {
     public class Account {
-        public decimal Balance { get; private set; }
-        internal bool IsVerified { get; private set; }
-        internal bool IsClosed { get; private set; }
-        internal bool IsFrozen { get; private set; }
-        private Action OnUnfreeze { get; }
-        private Action ManageUnfreezing { get; set; }
+        public decimal Balance { get; private set; }        
+        private IState State { get; set; }
 
         public Account(decimal startingBalance, Action onUnfreeze) {
             Balance = startingBalance;
-            OnUnfreeze = onUnfreeze;
-            ManageUnfreezing = StayUnfrozen;
+            State = new NotVerified(onUnfreeze);
         }
 
         public void Deposit(decimal amount) {
-            if (IsClosed)
-                return;
-            ManageUnfreezing();
-
-            Balance += amount;
+            State = State.Deposit(() => Balance += amount);            
         }
 
         public void Withdraw(decimal amount) {
-            if (IsClosed)
-                return;
-            if (!IsVerified)
-                return;
-            ManageUnfreezing();
-
-            Balance -= amount;
+            State = State.Withdraw(() => Balance -= amount);
         }
 
         public void HolderVerified() {
-            IsVerified = true;
+            State = State.HolderVerified();
         }
 
         public void Close() {
-            IsClosed = true;
+            State = State.Close();
         }
 
         public void Freeze() {
-            if (IsClosed)
-                return;
-            if (!IsVerified)
-                return;
-            IsFrozen = true;
-            ManageUnfreezing = Unfreeze;
-        }
-
-        private void Unfreeze() {
-            IsFrozen = false;
-            OnUnfreeze();
-            ManageUnfreezing = StayUnfrozen;
-        }
-
-        private void StayUnfrozen() { }
+            State = State.Freeze();
+        }        
     }
 }
